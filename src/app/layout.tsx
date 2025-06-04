92bd2dd1a17ae47c1593eace72582ac48a0ab797
binu <binu.b@hlbhamt.com>
2025-06-04 05:50:01 +0000

    need light mode by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className="font-body antialiased flex flex-col min-h-screen">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
